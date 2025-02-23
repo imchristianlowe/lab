7e22937344b4dd3be1a104ade232cdf38b23b586
banananeer <banananeer@gmail.com>
2025-02-23 13:03:17 -0500

    initial auth setup

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,13 +1,13 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-import App from "@/app/AuthScreen";
+import {SessionProvider} from "@/ctx";
+import {Slot, Stack} from "expo-router";
+import {StatusBar} from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -30,12 +30,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <App />
-      {/*<Stack>*/}
-      {/*  <Stack.Screen name="(tabs)" options={{ headerShown: false }} />*/}
-      {/*  <Stack.Screen name="+not-found" />*/}
-      {/*</Stack>*/}
-      {/*<StatusBar style="auto" />*/}
+      <SessionProvider>
+        <Slot />
+      </SessionProvider>
     </ThemeProvider>
   );
 }
