fcbb152a70b205ea3333dc00d08463094203e395
banananeer <banananeer@gmail.com>
2025-02-26 16:38:14 -0500

    setup app axios

--- frontend/app/(authenticated)/index.tsx
+++ frontend/app/(authenticated)/index.tsx
@@ -1,9 +1,28 @@
 import { Text, View } from "react-native";
 
-import { useSession } from "../../ctx";
+import { useSession } from "@/ctx";
+import useAppAxios from "@/hooks/useAppAxios";
+import { useState } from "react";
 
 export default function Index() {
   const { signOut } = useSession();
+
+  let [message, setMessage] = useState("");
+
+  const axios = useAppAxios();
+
+  async function validateToken(): Promise<void> {
+    let response = await axios.get(
+      "http://localhost:8000/validate_apple_id_token/",
+    );
+
+    if (response.data) {
+      setMessage(response.data.message);
+    }
+  }
+
+  validateToken();
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text
@@ -15,6 +34,7 @@ export default function Index() {
       >
         Sign Out
       </Text>
+      <Text style={{ color: "white" }}>{message}</Text>
     </View>
   );
 }
