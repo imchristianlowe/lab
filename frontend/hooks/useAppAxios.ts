bb1f4eb57125579872c022933e3232084ebac618
Christian Lowe <47257013+imchristianlowe@users.noreply.github.com>
2025-02-26 16:40:52 -0500

    Axios interceptor (#4)
    * update docs
    
    * setup app axios
    
    * update message when logged in

---
+++ frontend/hooks/useAppAxios.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosInstance } from "axios";
+import { useSession } from "@/ctx";
+
+export default function useAppAxios(): AxiosInstance {
+  const { session } = useSession();
+
+  let instance: AxiosInstance = axios.create();
+
+  instance.interceptors.request.use(
+    function (config) {
+      if (session) {
+        const sessionInfo = JSON.parse(session);
+        config.headers["X-Apple-Id-Token"] = sessionInfo["identityToken"];
+      }
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    },
+  );
+  return instance;
+}
