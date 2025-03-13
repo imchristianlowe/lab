50dedc8d56602c017eeadabf8b51a0c212995dc2
imchristianlowe <imchristianlowe@gmail.com>
2025-03-13 16:54:26 -0400

    create iam role for s3 uploads

---
+++ infra/iam.tf
@@ -0,0 +1,84 @@
+resource "aws_iam_role" "s3_backups" {
+  name = "s3-backups"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+       {
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": module.iam_user.iam_user_arn
+        },
+        "Action": "sts:AssumeRole"
+      },
+      {
+        "Effect": "Deny",
+        "Principal": {
+          "AWS": module.iam_user.iam_user_arn
+        },
+        "Action": "sts:AssumeRole",
+        "Condition": {
+          "NotIpAddress": {
+            "aws:SourceIp": [
+              "71.86.96.205/32",
+            ]
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "s3_backup" {
+  name = "s3-backup"
+  policy = data.aws_iam_policy_document.s3_backup_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "s3_backup" {
+  role = aws_iam_role.s3_backups.name
+  policy_arn = aws_iam_policy.s3_backup.arn
+}
+
+data "aws_iam_policy_document" "s3_backup_policy" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "s3:*",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "user_policy_document" {
+  statement {
+    sid = "1"
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+}
+
+
+resource "aws_iam_user_policy" "s3_backup_user_policy" {
+  name = "test"
+  user = module.iam_user.iam_user_name
+  policy = data.aws_iam_policy_document.user_policy_document.json
+}
+
+module "iam_user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+
+  name          = "s3-backups"
+  force_destroy = true
+
+  password_reset_required = false
+  create_iam_user_login_profile = false
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -5,3 +5,16 @@ output "cloudflare_zero_trust_tunnel_cloudflared_tunnel_name" {
 output "cloudflare_zero_trust_tunnel_cloudflared_tunnel_token" {
   value = data.cloudflare_zero_trust_tunnel_cloudflared_token.this
 }
+
+output "s3_backup_role_arn" {
+  value = aws_iam_role.s3_backups.arn
+}
+
+output "s3_backup_access_key_id" {
+  value = module.iam_user.iam_access_key_id
+}
+
+output "s3_backup_access_key_secret" {
+  value = module.iam_user.iam_access_key_secret
+  sensitive = true
+}
