7f059d9e4ff83eb83a0513969abb356692a629d9
imchristianlowe <imchristianlowe@gmail.com>
2025-03-22 10:34:03 -0400

    pass arbitrary props

--- expo/components/form/TextInputWrapper.tsx
+++ expo/components/form/TextInputWrapper.tsx
@@ -1,14 +1,24 @@
 import { useController } from "react-hook-form";
 import { TextInput } from "react-native";
+import { TextInputProps } from "react-native/Libraries/Components/TextInput/TextInput";
+import { Control } from "react-hook-form/dist/types/form";
 
-export const TextInputWrapper = ({ name, control, placeholder }) => {
+export const TextInputWrapper = ({
+  name,
+  control,
+  ...textInputProps
+}: {
+  name: string;
+  control: Control;
+  textInputProps?: TextInputProps | undefined;
+}) => {
   const { field } = useController({ control, defaultValue: "", name });
   return (
     <TextInput
       value={field.value}
       onChangeText={field.onChange}
       style={styles.input}
-      placeholder={placeholder}
+      {...textInputProps}
     />
   );
 };
