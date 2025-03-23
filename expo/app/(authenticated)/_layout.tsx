cf6f96f393b7ca5829dcda1001f9265dece6cda3
imchristianlowe <imchristianlowe@gmail.com>
2025-03-23 09:27:51 -0400

    add tab navigation

--- expo/app/(authenticated)/_layout.tsx
+++ expo/app/(authenticated)/_layout.tsx
@@ -1,10 +1,17 @@
-import { Text } from "react-native";
-import { Redirect, Stack } from "expo-router";
+import { Platform, Text } from "react-native";
+import { Redirect, Stack, Tabs } from "expo-router";
 
 import { useSession } from "@/ctx";
+import { Colors } from "@/constants/Colors";
+import { HapticTab } from "@/components/HapticTab";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { IconSymbol } from "@/components/ui/IconSymbol";
+import React from "react";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function AppLayout() {
   const { session, isLoading } = useSession();
+  const colorScheme = useColorScheme();
 
   // You can keep the splash screen open, or render a loading screen like we do here.
   if (isLoading) {
@@ -19,5 +26,40 @@ export default function AppLayout() {
     return <Redirect href="/sign-in" />;
   }
 
-  return <Stack />;
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarBackground: TabBarBackground,
+        tabBarStyle: Platform.select({
+          ios: {
+            // Use a transparent background on iOS to show the blur effect
+            position: "absolute",
+          },
+          default: {},
+        }),
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="house.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="person.fill" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
