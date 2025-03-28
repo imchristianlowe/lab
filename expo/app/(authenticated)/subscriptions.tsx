9f403fe62a0e6a2608d088e46a59a7092a1680b1
imchristianlowe <imchristianlowe@gmail.com>
2025-03-28 15:56:47 -0400

    setup revenue cat provider

--- expo/app/(authenticated)/subscriptions.tsx
+++ expo/app/(authenticated)/subscriptions.tsx
@@ -1,47 +1,82 @@
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
-import Purchases, { LOG_LEVEL, PurchasesPackage } from "react-native-purchases";
+import { PurchasesPackage } from "react-native-purchases";
 import { useEffect, useState } from "react";
-import React from "react";
-
-const APIKeys = {
-  apple: "appl_utMqkhIMipWFvYBgpmexWwQEPSp",
-};
+import { TouchableOpacity, StyleSheet } from "react-native";
+import { useRevenueCat } from "@/providers/RevenueCatProvider";
 
 export default function Subscriptions() {
   const [currentOfferings, setCurrentOfferings] = useState<
     PurchasesPackage[] | undefined
   >(undefined);
 
-  useEffect(() => {
-    const setup = async () => {
-      await Purchases.configure({ apiKey: APIKeys.apple });
-      try {
-        const offerings = await Purchases.getOfferings();
-        setCurrentOfferings(offerings.current?.availablePackages);
-      } catch (e) {
-        console.log(`Error ${e}`);
-        alert(e);
-      }
-    };
+  const { user, packages, purchasePackage, restorePermissions } =
+    useRevenueCat();
 
-    Purchases.setLogLevel(LOG_LEVEL.DEBUG);
+  const onPurchase = (pack: PurchasesPackage) => {
+    // Purchase the package
+    purchasePackage!(pack);
+  };
 
-    setup().catch(console.log);
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <ThemedView
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
       <ThemedText>Subscriptions Page</ThemedText>
-      {currentOfferings?.map((pack, index) => (
-        <React.Fragment key={`pack-${index}`}>
-          <ThemedText>{pack.product.title}</ThemedText>
-          <ThemedText>{pack.product.description}</ThemedText>
-          <ThemedText>{pack.product.priceString}</ThemedText>
-        </React.Fragment>
+      {packages?.map((pack, index) => (
+        // <TouchableOpacity key={`pack-${index}`} style={{flexDirection: 'column', alignItems: 'flex-start', marginVertical: 6}}>
+        //   <ThemedText>{pack.product.title}</ThemedText>
+        //   <ThemedText>{pack.product.description}</ThemedText>
+        //   <ThemedText>{pack.product.priceString}</ThemedText>
+        // </TouchableOpacity>
+        <TouchableOpacity
+          key={pack.identifier}
+          onPress={() => onPurchase(pack)}
+          style={styles.button}
+        >
+          <ThemedView style={styles.text}>
+            <ThemedText>{pack.product.title}</ThemedText>
+            <ThemedText style={styles.desc}>
+              {pack.product.description}
+            </ThemedText>
+          </ThemedView>
+          <ThemedView style={styles.price}>
+            <ThemedText>{pack.product.priceString}</ThemedText>
+          </ThemedView>
+        </TouchableOpacity>
       ))}
     </ThemedView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    alignItems: "flex-start",
+    marginVertical: 6,
+  },
+  button: {
+    padding: 12,
+    borderRadius: 4,
+    margin: 4,
+    flexDirection: "row",
+    width: "100%",
+    backgroundColor: "#fff",
+  },
+  text: {
+    flexGrow: 1,
+  },
+  desc: {
+    color: "#B6B7C0",
+    paddingVertical: 4,
+  },
+  price: {
+    justifyContent: "center",
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    borderColor: "#EA3C4A",
+  },
+});
