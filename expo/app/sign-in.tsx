35fa4f01b3cc84a23adbce13169b430b8d1a3448
imchristianlowe <imchristianlowe@gmail.com>
2025-04-01 16:42:59 -0400

    add sign in and sign up pages

--- expo/app/sign-in.tsx
+++ expo/app/sign-in.tsx
@@ -1,44 +1,72 @@
-import { router } from "expo-router";
-import { StyleSheet } from "react-native";
+import { Redirect, router, useRouter } from "expo-router";
+import { Button, Platform, StyleSheet, View } from "react-native";
 
 import { useSession } from "@/providers/AuthProvider";
 import * as AppleAuthentication from "expo-apple-authentication";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { ThemedTextInputWrapper } from "@/components/form/ThemedTextInputWrapper";
+import { useForm } from "react-hook-form";
 
 export default function SignIn() {
   const { signIn } = useSession();
+  const { control, handleSubmit } = useForm();
+  const router = useRouter();
   return (
     <ThemedView
       style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
     >
-      <AppleAuthentication.AppleAuthenticationButton
-        buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
-        buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
-        cornerRadius={5}
-        style={styles.button}
-        onPress={async () => {
-          try {
-            const credential = await AppleAuthentication.signInAsync({
-              requestedScopes: [
-                AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-                AppleAuthentication.AppleAuthenticationScope.EMAIL,
-              ],
-            });
-            signIn(JSON.stringify(credential));
-            console.log(credential);
-            router.replace("/");
-            // signed in
-          } catch (e: any) {
-            if (e.code === "ERR_REQUEST_CANCELED") {
-              // handle that the user canceled the sign-in flow
-            } else {
-              // handle other errors
+      <View width={10000}>
+        <ThemedTextInputWrapper
+          name={"username"}
+          control={control}
+          placeholder={"Username"}
+        />
+        <ThemedTextInputWrapper
+          name={"password"}
+          control={control}
+          placeholder={"Password"}
+          secureTextEntry={true}
+        />
+      </View>
+      <View>
+        <Button title={"Login"} />
+        <Button
+          title={"Sign Up"}
+          onPress={() => {
+            router.navigate("/sign-up");
+          }}
+        />
+      </View>
+
+      {Platform.OS === "ios" && (
+        <AppleAuthentication.AppleAuthenticationButton
+          buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
+          buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
+          cornerRadius={5}
+          style={styles.button}
+          onPress={async () => {
+            try {
+              const credential = await AppleAuthentication.signInAsync({
+                requestedScopes: [
+                  AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+                  AppleAuthentication.AppleAuthenticationScope.EMAIL,
+                ],
+              });
+              signIn(JSON.stringify(credential));
+              console.log(credential);
+              router.replace("/");
+              // signed in
+            } catch (e: any) {
+              if (e.code === "ERR_REQUEST_CANCELED") {
+                // handle that the user canceled the sign-in flow
+              } else {
+                // handle other errors
+              }
             }
-          }
-        }}
-      />
-      <ThemedText>Some random text</ThemedText>
+          }}
+        />
+      )}
     </ThemedView>
   );
 }
