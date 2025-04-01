32da64555d0612d61709051e0e4a8b1f9cefa2e0
imchristianlowe <imchristianlowe@gmail.com>
2025-04-01 17:56:29 -0400

    remove unneeded files

--- drf/drf_user_profile/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-
-from drf_user_profile import views
-
-router = DefaultRouter()
-
-router.register(r"users", views.UserViewSet)
-
-urlpatterns = [] + router.urls
