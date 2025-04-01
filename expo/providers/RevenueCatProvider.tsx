46ac0533c07e1b844bc9272adb28b4ea927d207d
imchristianlowe <imchristianlowe@gmail.com>
2025-04-01 16:05:26 -0400

    remove unnecessary code

--- expo/providers/RevenueCatProvider.tsx
+++ expo/providers/RevenueCatProvider.tsx
@@ -1,12 +1,7 @@
 // Provide RevenueCat functions to our app
 import { createContext, useContext, useEffect, useState } from "react";
-import {
-  CustomerInfo,
-  LOG_LEVEL,
-  PurchasesPackage,
-} from "react-native-purchases";
+import { CustomerInfo, PurchasesPackage } from "react-native-purchases";
 import { Platform } from "react-native";
-import { PurchasesError } from "@revenuecat/purchases-js";
 
 // Import React Native Purchases for native platforms
 let Purchases;
@@ -18,7 +13,7 @@ if (Platform.OS !== "web") {
 let RevenueCatWeb;
 if (Platform.OS === "web") {
   // You'll need to install and import the RevenueCat JS SDK
-  Purchases = require("@revenuecat/purchases-js").Purchases;
+  RevenueCatWeb = require("@revenuecat/purchases-js").Purchases;
 }
 
 const APIKeys = {
@@ -63,17 +58,10 @@ export const RevenueCatProvider = ({ children }: any) => {
       } else if (Platform.OS === "ios") {
         await Purchases.configure({ apiKey: APIKeys.apple });
       } else {
-        await Purchases.configure(APIKeys.web, "something");
+        await RevenueCatWeb.configure(APIKeys.web, "something");
+        Purchases = RevenueCatWeb.getSharedInstance();
       }
       setIsReady(true);
-
-      // Use more logging during debug if want!
-      Purchases.setLogLevel(LOG_LEVEL.DEBUG);
-
-      // // Listen for customer updates
-      // Purchases.addCustomerInfoUpdateListener(async (info) => {
-      //   updateCustomerInformation(info);
-      // });
 
       // Load all offerings and the user object with entitlements
       await loadOfferings();
@@ -83,50 +71,16 @@ export const RevenueCatProvider = ({ children }: any) => {
 
   // Load all offerings a user can (currently) purchase
   const loadOfferings = async () => {
-    console.log("loading products");
-    if (Platform.OS !== "web") {
-      const offerings = await Purchases.getOfferings();
-      if (offerings.current) {
-        setPackages(offerings.current.availablePackages);
-      }
-    } else {
-      const products = await Purchases.getSharedInstance().getOfferings();
-      console.log("products", products);
+    const offerings = await Purchases.getOfferings();
+    if (offerings.current) {
+      setPackages(offerings.current.availablePackages);
     }
-  };
-
-  // Update user state based on previous purchases
-  const updateCustomerInformation = async (customerInfo: CustomerInfo) => {
-    const newUser: UserState = { cookies: user.cookies, items: [], pro: false };
-
-    if (customerInfo?.entitlements.active["Epic Wand"] !== undefined) {
-      newUser.items.push(
-        customerInfo?.entitlements.active["Epic Wand"].identifier,
-      );
-    }
-
-    if (customerInfo?.entitlements.active["Magic Boots"] !== undefined) {
-      newUser.items.push(
-        customerInfo?.entitlements.active["Magic Boots"].identifier,
-      );
-    }
-
-    if (customerInfo?.entitlements.active["PRO Features"] !== undefined) {
-      newUser.pro = true;
-    }
-
-    setUser(newUser);
   };
 
   // Purchase a package
   const purchasePackage = async (pack: PurchasesPackage) => {
     try {
       await Purchases.purchasePackage(pack);
-
-      // Directly add our consumable product
-      if (pack.product.identifier === "rca_299_consume") {
-        setUser({ ...user, cookies: (user.cookies += 5) });
-      }
     } catch (e: any) {
       if (!e.userCancelled) {
         alert(e);
