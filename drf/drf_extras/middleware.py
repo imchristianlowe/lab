9d3255921bb2d262f9e7161f9e459eed2c8bffe0
imchristianlowe <imchristianlowe@gmail.com>
2025-04-03 10:25:39 -0400

    work with arrays

--- drf/drf_extras/middleware.py
+++ drf/drf_extras/middleware.py
@@ -2,6 +2,7 @@ import logging
 import socket
 import time
 
+from django.http import JsonResponse
 
 logger = logging.getLogger(__name__)
 
@@ -15,15 +16,13 @@ class RequestLogMiddleware:
         extra = self.process_request(request)
 
         response = self.get_response(request)
-
         end_time = time.time()
 
         extra["elapsed_time"] = end_time - start_time
         extra["response_code"] = response.status_code
         extra["user"] = request.user.pk
-
         logger.info(
-            f"API Request {response.status_code}, {request.get_full_path()} {response.data}",
+            f"API Request {response.status_code}, {request.get_full_path()}",
             extra=extra,
         )
         return response
