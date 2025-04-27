432d846c3638c41f153deb450d95daf6dc432b7b
imchristianlowe <imchristianlowe@gmail.com>
2025-04-27 12:10:25 -0400

    add prefix

--- docs/js/loader.js
+++ docs/js/loader.js
@@ -3,10 +3,11 @@ import "./mermaid.min.js";
 mermaid.registerIconPacks([
   {
     name: "logos",
-    loader: () => fetch("/static/icons/logos.json").then((res) => res.json()),
+    loader: () =>
+      fetch("/lab/static/icons/logos.json").then((res) => res.json()),
   },
   {
     name: "mdi",
-    loader: () => fetch("/static/icons/mdi.json").then((res) => res.json()),
+    loader: () => fetch("/lab/static/icons/mdi.json").then((res) => res.json()),
   },
 ]);
